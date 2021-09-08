81d05cbc625a2481db4da04a09b6c1b060eefe29
paulinemarg <paulinemargossian@gmail.com>
2021-09-08 20:23:55 +0200

    Update scripts.js
    changed functions

--- scripts.js
+++ scripts.js
@@ -1,37 +1,18 @@
-
-function newItem(){
-  //1. Adding a new item to the list of items:
-  let li = $('<li></li>');
-  let inputValue = $('#input').val();
-  li.append(inputValue);
-
-
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    $('#list').append(li);
-  }
-
-  //2. Crossing out an item from the list of items:
-  function crossOut() {
-    li.toggleClass("strike");
-  }
-  li.on("dblclick", function (crossOut) {
-    li.toggleClass("strike")
+$(document).ready(function () {
+  // Add task 
+  $('input').change(function () {
+    var input = $(this).val();
+    $('ul').append('<li>' + input + ' <i class="fa fa-check"></i> <i class="fa fa-trash"></i> </li>');
+    $(this).val('');
   });
 
-  //3(i). Adding the delete button "X":
-  let crossOutButton = $('<crossOutButton></crossOutButton>');
-  crossOutButton.append(document.createTextNode("X"));
-  li.append(crossOutButton);
+  // delete task
+  $('ul').on('click', '.fa-trash', function () {
+    $(this).parent('li').fadeOut(200);
+  })
 
-  crossOutButton.on("click", deleteListItem);
-
-  //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-  function deleteListItem(){
-    li.addClass("delete")
-  }
-  // 4. Reordering the items:
-  $('#list').sortable();
-
-}
+  // Mark as done with line through
+  $('ul').on('click', '.fa-check', function () {
+    $(this).parent('li').toggleClass('checked');
+  })
+});
